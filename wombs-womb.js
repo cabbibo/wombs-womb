94751b1e84735b8ac89575f20e8c7edec9c7caab
Isaac Cohen <icohen@leapmotion.com>
2014-05-11 20:12:54 -0700

    updating wombs-womb to first working version. LONG WAY TO GO

--- wombs-womb.js
+++ wombs-womb.js
@@ -3,9 +3,18 @@
   var THREE     = require( 'three'                  );
   var Component = require( 'wombs-three-component'  );
 
+  var audioController = require( 'wombs-audio-controller' );
 
   var mutation = {}
 
+  /*
+  
+     TODO:
+     remakes everything with certain params,
+     in order to be able to personalize womb
+     ( size , fov , failure cases , require techs , etc );
+
+  */
   mutation.recreate = function( params ){
 
     this.params = _.defaults( params || {} , {
@@ -14,6 +23,8 @@
     });
 
   }
+
+
   Womb.prototype = _.extend( 
     Component.prototype,
     mutation
@@ -24,10 +35,49 @@
 
     var scene = new THREE.Scene();
 
-    Component.call( this , scene ); 
+    Component.call( this , scene );
+
+
+    
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var size = 100;
+
+
+    var camera = new THREE.PerspectiveCamera( 40 , width / height , 1 , 1000 );
+    camera.position.z = 100;
+     
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( width , height );
+
+    document.body.style.margin = '0';
+    document.body.appendChild( renderer.domElement );
+
+    this.audioController = audioController;
+    this.addComponent( audioController );
+
+
+    /*
+
+       TODO:
+
+       - Events
+       - Loading
+       - Animator(?)
+       - Interface
+
+    */
+
+    this.addToUpdateArray( function(){
+
+      renderer.render( scene , camera );
+
+    });
 
   }
 
 
-  var womb = Womb();
 
+  var womb = new Womb();
+
+  module.exports = womb;
